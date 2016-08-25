0f6327f215b7dc3fdce3caa27400750ee2654631
方光明 <563508194@qq.com>
2016-08-25 09:33:05 +0800

    提交alpha分之测试

--- src/main/java/com/demo/jfinal/start/JFinalStart.java
+++ src/main/java/com/demo/jfinal/start/JFinalStart.java
@@ -9,6 +9,8 @@ import com.jfinal.core.JFinal;
 public class JFinalStart {
 
     public static void main(String []args){
+
+         //这是alpha分之提交的代码
         JFinal.start("src/main/webapp",8080,"/",5);
 
     }
